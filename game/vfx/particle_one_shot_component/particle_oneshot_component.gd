6d7d11202549c35469296b7ac14212b71b568694
Maurice Huchler <maurice.huchler@hs-furtwangen.de>
2025-09-03 17:48:04 +0200

    update particle component MORE AUTOMATION!!!!

--- game/vfx/particle_one_shot_component/particle_oneshot_component.gd
+++ game/vfx/particle_one_shot_component/particle_oneshot_component.gd
@@ -1,11 +1,11 @@
 extends Node
 @export var kill_time: float  = 1.0
-@export var particles_to_start: Array[GPUParticles3D]
 @export var node_to_kill: Node
 
 var timer: Timer
 
 func _ready() -> void:
+	var particles_to_start = find_particles()
 	for particles in particles_to_start:
 		particles.emitting = true
 	timer = Timer.new()
@@ -13,5 +13,10 @@ func _ready() -> void:
 	add_child(timer)
 	timer.start(kill_time)
 	
-	
-	
+func find_particles() -> Array:
+	var particles: Array
+	var children = get_parent().get_children()
+	for child in children:
+		if child is GPUParticles3D or child is CPUParticles3D:
+			particles.append(child)
+	return particles
