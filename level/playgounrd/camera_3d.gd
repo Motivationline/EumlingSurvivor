45d95dcf981571899f843bb9cecb9159a4bee6a0
Maurice Huchler <maurice.huchler@hs-furtwangen.de>
2025-09-10 14:12:12 +0200

    lvl001 update + camera script

--- level/playgounrd/camera_3d.gd
+++ level/playgounrd/camera_3d.gd
@@ -5,10 +5,12 @@ var offset_x = 0.0
 var offset_z = 0.0
 
 func _ready() -> void:
-	offset_x = position.x
-	offset_z = position.z
+	player = get_tree().get_nodes_in_group("Player")[0]
+	offset_x =position.x
+	offset_z =position.z
 func _process(delta: float) -> void:
 	pass
 func _physics_process(delta: float) -> void:
-	global_position.x = lerp(global_position.x,player.global_position.x +offset_x,0.1)
-	global_position.z = lerp(global_position.z,player.global_position.z + offset_z,0.1)
+	if player != null:
+		global_position.x = lerp(global_position.x,player.global_position.x +offset_x,0.1)
+		global_position.z = lerp(global_position.z,player.global_position.z + offset_z,0.1)
