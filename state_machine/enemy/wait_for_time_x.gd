72dbbe12027544a49b8e66c1f2493c9890ef3661
M00St4r <64206620+M00St4r@users.noreply.github.com>
2025-09-12 11:59:59 +0200

    add wait for Time x

--- state_machine/enemy/wait_for_time_x.gd
+++ state_machine/enemy/wait_for_time_x.gd
@@ -1,36 +1,41 @@
 @tool
 extends State
-## Picks a random State from all StateMachine Childs, self excluded
-class_name PickRandomState
+## Let The Entity wait
+class_name WaitForTimeX
 
-## optional wait Time until the next State starts
-@export var wait_time: float = 0:
+## Wait Time, default 1 sec
+@export var wait_time: float = 1:
 	set(new_value):
 		wait_time = max(new_value, 0)
+
+## optional random Wait Time overwrite
+@export_category("Random Wait Time")
+
+@export var random: bool = false
+
+## default 0
+@export var min: float = 0
+## default 1
+@export var max: float = 1
 
 var done: bool = false
 
 func setup(_parent: Enemy, _animation_tree: AnimationTree):
 	super (_parent, _animation_tree)
 	done = false
-	next_state = self
 
 func enter():
 	super()
 	done = false
-	next_state = self
-	pick_random_state()
+	wait()
 
 func physics_process(_delta: float) -> State:
 	if(done) : return return_next()
 	return null
 
-func pick_random_state():
-	var states: Array[Node] = get_parent().get_children()
-	while next_state is PickRandomState:
-		var selector = randi_range(0, len(states)-1)
-		next_state = states[selector]
-	#print(next_state)
+func wait():
+	if random:
+		wait_time = randf_range(min, max)
 	await get_tree().create_timer(wait_time).timeout
 	done = true
 	
