5b3df9e40fe5de9a94dfc52885a39dea85015b45
Plagiatus <lukas.scheuerle@web.de>
2025-09-12 15:11:52 +0200

    countdown strategy

---
+++ strategies/event/countdown.gd
@@ -0,0 +1,26 @@
+extends EventStrategy
+## Counts down every time this event is called before triggering the attached event when countdown reaches 0
+class_name CountdownEventStrategy
+
+## How often this event needs to be triggered before the defined event is called
+@export_range(1, 100) var count: int = 1
+var current_count: int
+
+## What event to call once the count is up
+@export var event: EventStrategy
+
+## Whether the countdown should only happen once or over and over
+@export var once: bool = false
+var happened: bool = false
+
+func _setup(_parent: Node):
+	super (_parent)
+	current_count = count
+
+func event_triggered(_data):
+	if (once && happened): return
+	current_count -= 1
+	if (current_count == 0):
+			current_count = count
+			event.event_triggered(_data)
+			happened = true
