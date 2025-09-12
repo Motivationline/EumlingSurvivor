1b1d9b4bf96e13489ecf067ece8aa357c8d43b21
Plagiatus <lukas.scheuerle@web.de>
2025-09-12 18:39:15 +0200

    better countdown count explanation

--- strategies/event/countdown.gd
+++ strategies/event/countdown.gd
@@ -2,7 +2,9 @@ extends EventStrategy
 ## Counts down every time this event is called before triggering the attached event when countdown reaches 0
 class_name CountdownEventStrategy
 
-## How often this event needs to be triggered before the defined event is called
+## How often this event needs to be triggered before the defined event is called.[br]  
+## Or in other words: every n-th time this event is called, call the other event.[br]
+## 1 -> every time, 2 -> every other time, etc.
 @export_range(1, 100) var count: int = 1
 var current_count: int
 
