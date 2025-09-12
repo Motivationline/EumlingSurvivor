29eda5a92431d9d57da4dc90a37cbdef6c9636f8
Plagiatus <lukas.scheuerle@web.de>
2025-09-12 15:22:22 +0200

    seems like the default targeting is the wrong direction

--- game/spawner/strategies/target/target.gd
+++ game/spawner/strategies/target/target.gd
@@ -1,4 +1,4 @@
 extends Resource
 class_name TargetStrategy
 
-func get_target_position(_parent: Node3D) -> Vector3: return _parent.global_position + _parent.basis.z
+func get_target_position(_parent: Node3D) -> Vector3: return _parent.global_position - _parent.basis.z
