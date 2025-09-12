62245369c3ca94d501f89b2243721e328827c7a8
Plagiatus <lukas.scheuerle@web.de>
2025-09-12 10:31:27 +0200

    add modes to projectile rotation movement

--- strategies/projectile_movement/rotation.gd
+++ strategies/projectile_movement/rotation.gd
@@ -5,6 +5,9 @@ class_name RotationProjectileMovementStrategy
 ## Ignores the max_domain of the curve and assumes it describes the entire lifetime.
 @export var normalize_to_lifetime: bool = true
 
+## Should the values from the curve be used to [b]set[/b] the rotation (based on start rotation) or to [b]add[/b] to the rotation every frame. 
+@export_enum("set", "add") var mode = "set"
+
 ## In degrees
 @export var rotation: Curve
 
@@ -20,4 +23,7 @@ func apply_movement(_delta: float, _current_lifetime: float, _total_lifetime: fl
 	if (!rotation): return
 	var sample_point = (_current_lifetime / _total_lifetime) * rotation.max_domain if (normalize_to_lifetime) else fmod(_current_lifetime, rotation.max_domain)
 	var current_rotation = rotation.sample(sample_point)
-	parent.rotation_degrees.y = current_rotation + start_rotation
+	if (mode == "set"):
+		parent.rotation_degrees.y = current_rotation + start_rotation
+	elif (mode == "add"):
+		parent.rotation_degrees.y += current_rotation
