bff9d69a8489742833cdd4179f2cfa0f55ddd002
Plagiatus <lukas.scheuerle@web.de>
2025-09-15 10:58:38 +0200

    player don't rotate AOEs, upgrades prep

--- game/player/player.gd
+++ game/player/player.gd
@@ -4,6 +4,10 @@ class_name Player
 @export var speed := 5.0
 const BULLET = preload("res://game/projectiles/example_bullet/example_bullet.tscn")
 @export var spawner: EntitySpawner
+@export var eumling_visuals: Node3D
+
+var active_upgrades: Dictionary[Enum.UPGRADE, Array] = {}
+signal upgrade_added
 
 func _ready() -> void:
 	add_to_group("Player")
@@ -16,7 +20,7 @@ func _physics_process(_delta: float) -> void:
 
 	
 	if (!direction.is_zero_approx()):
-		look_at(global_position + direction_3d)
+		eumling_visuals.look_at(global_position + direction_3d)
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("test")):
@@ -26,4 +30,14 @@ func spawn_bullet():
 	# var instance = BULLET.instantiate()
 	# get_parent().add_child(instance)
 	# instance.setup(Enum.GROUP.PLAYER, global_position, global_rotation, global_position + basis.z * -5)
-	spawner.spawn(self)
+	spawner.spawn(self, eumling_visuals)
+
+func add_upgrade(upgrade: UpgradeStrategy):
+	var upgrades = get_upgrades_for(upgrade.type)
+	upgrades.append(upgrade)
+	active_upgrades.set(upgrade.type, upgrades)
+	upgrade_added.emit(upgrade)
+
+func get_upgrades_for(type: Enum.UPGRADE) -> Array[UpgradeStrategy]:
+	if (!active_upgrades.has(type)): return []
+	return active_upgrades.get(type)
