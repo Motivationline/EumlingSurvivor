cba1563d87a6b8f31600283f012e507453cebc1c
Maurice Huchler <maurice.huchler@hs-furtwangen.de>
2025-09-26 14:36:10 +0200

    animation trigger coponent

--- game/shared/animation trigger component/animation_trigger_component.gd
+++ game/shared/animation trigger component/animation_trigger_component.gd
@@ -4,11 +4,24 @@ extends Node
 @export var signal_to_connect: String
 @export var animation_player: AnimationPlayer
 @export var animation_to_play: String
+## use this if node is not in current scene
+@export var node_with_animationplayer: Node3D
 
 
 func _ready() -> void:
 	if node_to_connect != null and signal_to_connect != "":
-		var signal_list = node_to_connect.get_signal_list()
-		for sig in signal_list:
-			print(sig.get("args",[]))
-	
+		node_to_connect.connect(signal_to_connect,_play_animation)
+	if node_with_animationplayer != null:
+		animation_player = find_first_animation_tree(node_with_animationplayer)
+
+func find_first_animation_tree(node: Node3D) -> AnimationPlayer:
+	if (!node): return null
+	for child in node.get_children():
+		if (child is AnimationPlayer): return child
+		if (child.get_child_count() > 0 && child is Node3D):
+			var tree = find_first_animation_tree(child)
+			if (tree && tree is AnimationPlayer): return tree
+	return null
+
+func _play_animation(_a = null, _b = null, _c = null, _d = null, _e = null, _f = null, _h = null):
+	animation_player.play(animation_to_play)
