7ff79c3d139cea5d9cf6d2b714710552da2d1b5e
M00St4r <64206620+M00St4r@users.noreply.github.com>
2025-10-19 11:28:50 +0200

    work on univertsal targeting strat

--- strategies/projectile_target/universal.gd
+++ strategies/projectile_target/universal.gd
@@ -2,19 +2,52 @@ extends ProjectileTargetStrategy
 ## sets the target(s) based on inputs
 class_name UniversalTargetingProjectileTargetStrategy
 
+enum target_types { Player, Enemy }
+
 ## preferred type of entety or Node you want to target
-@export var target_type: Node
+@export var target_type: target_types
 ## the max distance in that we pick targets from
 @export_range(0,100,1) var max_radius: float
 ## the min distance in that we pick targets from
 @export_range(0,100,1) var min_radius: float
 ## preferred max target amount
-@export var max_targets: float
+@export var max_targets: int
 
 
 func find_target():
-	var enemies = get_tree().get_nodes_in_group("Enemy")
-	get_closest_Node(enemies)
+	
+	var targets: Array[Node]
+	
+	if target_type == target_types.Player:
+		pass
+	elif target_type == target_types.Enemy:
+		targets = get_tree().get_nodes_in_group("Enemy")
+		#get_closest_Node(targets)
+		#print("getting all targets", targets)
+	
+	_sort_targets_by_distance(targets)
+	
+	for target in targets:
+		if parent.global_transform.origin.distance_to(target.global_transform.origin) > max_radius:
+			var i = targets.find(target)
+			targets.pop_at(i)
+	
+	if len(targets) > max_targets:
+		targets = targets.slice(0, max_targets)
+	print("slicing targets", targets)
+	
+	parent.targets = targets
+	
+func _sort_targets_by_distance(_targets):
+	_targets.sort_custom(func(a, b): return _sort_by_distance(a, b))
+	
+func _sort_by_distance(a: Node, b: Node) -> bool:
+	var dist_a = parent.global_transform.origin.distance_to(a.global_transform.origin)
+	var dist_b = parent.global_transform.origin.distance_to(b.global_transform.origin)
+	return dist_a < dist_b
+
+
+
 
 # returns the closest Node from the given Array
 func get_closest_Node(_nodes: Array[Node]):
@@ -27,4 +60,4 @@ func get_closest_Node(_nodes: Array[Node]):
 			closest_dist = dist
 			closest = n
 	parent.targets.append(closest)#adds the closest to the targets array
-	print("Targets: ", parent.targets, "len: ", len(parent.targets), "idx_0: ", parent.targets[0])
+	#print("Targets: ", parent.targets, "len: ", len(parent.targets), "idx_0: ", parent.targets[0])
