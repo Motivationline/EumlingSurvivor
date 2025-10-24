a9e39088ff74d102f695238b02538a4b1105f36b
M00St4r <64206620+M00St4r@users.noreply.github.com>
2025-10-24 07:57:38 +0200

    target hit check

--- strategies/event/mark_target_as_hit.gd
+++ strategies/event/mark_target_as_hit.gd
@@ -5,5 +5,17 @@ class_name MarkTargetAsHitTargetsEventStrategy
 
 func event_triggered(_data):
 	if len(parent.targets) > 0:
-		parent._add_hit(parent.targets[0])
-		parent._remove_target(parent.targets[0])
+		var hit = arrays_overlap(parent.targets, parent.hit_box.overlapping)
+		if hit:
+			var i = parent.targets.find(hit)
+			parent._add_hit(hit)
+			parent._remove_target(parent.targets[i])
+
+func arrays_overlap(arr1, arr2):
+	var arr2_dict = {}
+	for node in arr2:
+		arr2_dict[node] = true
+	for node in arr1:
+		if arr2_dict.has(node):
+			return node
+	return false
