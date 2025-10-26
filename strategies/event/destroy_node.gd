764adfeb078f1a0d05326db0eab915b41d1adf88
M00St4r <64206620+M00St4r@users.noreply.github.com>
2025-10-26 10:00:18 +0100

    add test projectile

--- strategies/event/destroy_node.gd
+++ strategies/event/destroy_node.gd
@@ -12,9 +12,14 @@ class_name DestroyNodeEventStrategy
 ## after delay time has passed, the action is triggered
 @export var delay: float = 0
 
+var hit: bool = false
+
 func event_triggered(_data):
 	if is_delayed:
 		await get_tree().create_timer(delay).timeout
 		
-	for n in nodes:
-		n.queue_free()
+	#TODO: make this piercing dependant?
+	if not hit:
+		for n in nodes:
+			n.queue_free()
+		hit = true
