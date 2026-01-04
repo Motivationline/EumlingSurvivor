aa26511d0b0c479da8c04ed113e0d9e39356fc03
M00St4r <64206620+M00St4r@users.noreply.github.com>
2026-01-04 15:14:04 +0100

    play with death sound, add protocol

--- strategies/event/enemy/play_sound.gd
+++ strategies/event/enemy/play_sound.gd
@@ -1,7 +1,7 @@
 @tool
 extends EventStrategy
 ## Triggers a State from the State Machine
-class_name PlayHitSoundEventStrategy
+class_name PlaySoundEventStrategy
 
 @export var sound_manager: SoundEffectManager
 
@@ -11,3 +11,11 @@ func _ready() -> void:
 func event_triggered(_data):
 	sound_manager.play_sound(_data)
 	await sound_manager.sound_list[_data].finished
+
+
+# Protocol:
+#	DeactivateEvent only deactivates _process function
+#	event data is hardcoded in on_hurt and on_hit
+#	how to play sound? ?.?
+#	not compatible with strategy system?!
+#	await --------
