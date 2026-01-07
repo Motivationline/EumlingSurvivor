4573d84e8971c8d53b744d5cc13596c3a1ec6e85
Plagiatus <lukas.scheuerle@web.de>
2026-01-07 12:17:56 +0100

    fix paths in upgrade option

--- game/ui/upgrades/upgrade_option.gd
+++ game/ui/upgrades/upgrade_option.gd
@@ -1,11 +1,12 @@
 extends PanelContainer
 class_name UpgradeOption
 
-@onready var title: Label = $MarginContainer/VBoxContainer/Title
-@onready var subtitle: Label = $MarginContainer/VBoxContainer/Subtitle
-@onready var info: RichTextLabel = $MarginContainer/VBoxContainer/Info
+@onready var title: Label = $Sprite2D/Title
+@onready var subtitle: Label = $Sprite2D/Subtitle
+@onready var info: RichTextLabel = $Sprite2D/Info
 
 func setup(upgrade: Upgrade):
+	prints(Enum.UPGRADE.keys())
 	info.text = "%s: %s %s" % [Enum.UPGRADE.keys()[upgrade.type], Enum.UPGRADE_METHOD.keys()[upgrade.method], upgrade.value]
 	theme_type_variation = Enum.RARITY.keys()[upgrade.rarity]
 	
