aa74c6cebad79e8e7d35e4786f674e3c650edd9a
M00St4r <64206620+M00St4r@users.noreply.github.com>
2026-01-27 08:37:28 +0100

    track/predikt target location

--- strategies/projectile_movement/artillery_move_to_target.gd
+++ strategies/projectile_movement/artillery_move_to_target.gd
@@ -9,6 +9,8 @@ class_name ArtilleryToTargetProjectileMovementStrategy
 
 @export var speed_amp: float = 10
 
+@export var arc_factor: float = 2
+
 @onready var db_cube: MeshInstance3D = $"../../DBMesh"
 
 var isPosLocked: bool = false
@@ -16,6 +18,8 @@ var start_pos: Vector3
 var target_pos: Vector3
 var distance: float
 var traveled: float
+
+#var aviation_time: float = 0
 
 func _setup(_parent: Node, _owner: Node):
 	super (_parent, _owner)
@@ -29,41 +33,43 @@ func apply_movement(_delta: float, _current_lifetime: float, _total_lifetime: fl
 	#checks if the projectile is fired from the player and adjusts the target accordingly
 	if len(parent.get_targets()) > 0:
 		var target = parent.get_targets()[0]
-		#print("Target: ", target)
 		if target:
-			#var speed_mag = parent.velocity.length()
-			var new_dir = lerp(parent.velocity.normalized(), (target.position - parent.position).normalized(), _delta * velocity_change_rate)
-			#parent.velocity = lerp(parent.velocity, (target.position - parent.position).normalized() * speed, _delta * rotation_speed)
 			
 			#parabula y = x/z
 			#line: player -> targetpos
 			
+			#TODO: follow player movement?
+			
 			if not isPosLocked:
 				target_pos = target.position
 				distance = (target_pos - start_pos).length()
+				
+				var parab_length = calculate_parabua_length((-arc_factor / distance), arc_factor,0 , distance)
+				var travel_time = calculate_travel_time(parab_length, speed_amp)
+				# rougth estimate
+				var adjusted_target_position = target.position + target.velocity * travel_time
+				
+				print(target_pos, target.velocity, travel_time, adjusted_target_position)
+				
+				target_pos = adjusted_target_position
+				
 				isPosLocked = true
-				print("distance:", distance)
-				#target_pos.y = 0
-				#start_pos.y = 0
 			
-			db_cube.global_position = target_pos
+			# set debug cube pos
+			#db_cube.global_position = target_pos
 			
 			var parent_pos = parent.position
 			#parent_pos.y = 0
 			
 			traveled = ((start_pos - Vector3(0,start_pos.y,0))- (parent_pos - Vector3(0,parent_pos.y,0))).length()
-			print("traveled: ",traveled, " start_pos: ", start_pos, " target_pos: ", target_pos, " parent_pos: ", parent.global_position)
 			# x is the amount we already traveled in target position
 			var x = traveled
 			# calculate the y-pos for x (how far we traveled to the target) on the parabula
 			# the parabula ranges from start_pos to target_pos
-			var arc_factor = 2
-			var y = arc_factor/distance * (-(x * x) + distance * x)
+			var y = (-arc_factor / distance) * x * x + arc_factor * x
 			# calculate the gradient (steigung) at a certain point
-			# we can use the gradient to calculate the angle our projectile should be at on every point
+			# use the gradient to calculate the angle the projectile should be at on every point
 			var gradient = arc_factor/distance * (- 2*x + distance)
-			
-			#parent.velocity = new_dir #* speed_mag
 			
 			
 			# horizontal direction from start to target
@@ -78,10 +84,26 @@ func apply_movement(_delta: float, _current_lifetime: float, _total_lifetime: fl
 			var dir = horizontal_dir + Vector3.UP * slope
 			dir = dir.normalized()
 
-			#var speed = parent.velocity.length()
 			parent.velocity = dir * speed_amp
 			
-			#print("velocity: ", parent.velocity, ", new_dir: ", new_dir, ", speed_mag: ", speed_mag)
-			#parent.position.y = y
-			#parent.rotation.y = tan(1/gradient)
-			#parent.rotation.x = lerp_angle(parent.rotation.x, atan2(-parent.velocity.y, -parent.velocity.z), _delta * rotation_speed)
+			#aviation_time += _delta
+			#print(calculate_travel_time(parab_length, speed_amp))
+			#print(aviation_time)
+			
+			if parent.velocity.length() > 0:
+				parent.look_at(parent.global_position + parent.velocity)
+
+func calculate_travel_time(_length:float, _speed: float) -> float:
+	var t = _length/_speed
+	return t
+	
+
+func _F(x: float, a: float, b: float) -> float:
+	var u = 2.0 * a * x + b
+	return (u * sqrt(1.0 + u * u) + asinh(u)) / (4.0 * a)
+
+func calculate_parabua_length(a: float, b: float, x0: float, x1: float) -> float:
+	if abs(a) < 0.000001:
+		return sqrt(1.0 + b * b) * abs(x1 - x0)
+
+	return abs(_F(x1, a, b) - _F(x0, a, b))
