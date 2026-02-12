c92509389b1f2505f777bd8694296c847d83d61b
GammaRayTea <raymond.g.n.dinh@gmail.com>
2026-02-12 09:31:53 +0100

    small reveal sound update to allow polyphony

--- game/sound/sfx/sound_presets/sound_sequence.gd
+++ game/sound/sfx/sound_presets/sound_sequence.gd
@@ -4,10 +4,14 @@ class_name SoundSequence extends Node
 var counter:int = 0
 
 @export var audio_streams: Array[AudioStream]
+var playback :AudioStreamPlaybackPolyphonic
+func _ready():
+	audio_stream_player.play()
+	audio_stream_player
+	playback  = audio_stream_player.get_stream_playback()
 
 func play(from_position = 0.0) -> void:
-	audio_stream_player.stream = audio_streams[counter]
-	audio_stream_player.play(from_position)
+	playback.play_stream(audio_streams[counter])
 	counter+= 1
 	
 	if counter == audio_streams.size():
