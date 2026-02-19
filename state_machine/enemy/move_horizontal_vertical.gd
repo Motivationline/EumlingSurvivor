c2287248d9d211180398e29a45624c458235476b
M00St4r <64206620+M00St4r@users.noreply.github.com>
2026-02-19 20:15:03 +0100

    wierd rotation

--- state_machine/enemy/move_horizontal_vertical.gd
+++ state_machine/enemy/move_horizontal_vertical.gd
@@ -10,12 +10,8 @@ class_name MoveHorizontalVerticalState
 	set(new_value):
 		wait_time = max(new_value, 0)
 
-## true = vertical, false = horizontal
-@export var direction: bool
 ## at what distance the entity stops infront of level Geometry
 @export var stop_distance: float = 1
-## value to control how much the entity "straves"
-@export_range(0,360,1) var rotation_offset: float
 ## raycast to handle pathing
 @export var path_ray: RayCast3D
 
@@ -36,7 +32,8 @@ var move_direction: Vector3
 var ray_length = 30
 
 var pos1: Vector3
-var pos2: Vector3
+
+var target_position: Vector3
 
 func setup(_parent: Enemy, _animation_tree: AnimationTree):
 	super (_parent, _animation_tree)
@@ -46,67 +43,60 @@ func enter():
 	
 	#TODO: create 2 Raycast, shoot front and back, save collision points, move between them
 	
-	path_ray.global_position = parent.position
-	
-	move_direction = get_new_direction()
+	path_ray.global_position = parent.position + Vector3(0,0.5,0)
+	# move to target position
+	#move_direction = get_new_direction()
+	target_position = get_new_target_position()
 
 func physics_process(_delta: float) -> State:
 	if (done): return return_next()
 	
 	var speed = speed_override if (speed_override_active) else parent.speed
+	
+	move_direction = (target_position - parent.global_position).normalized()
+	
 	parent.velocity = move_direction * speed
 
 	parent.move_and_slide()
 	
 	# checks if the entity is in stop distance to the target position
-	if parent.global_position.distance_to(pos1) < stop_distance || parent.global_position.distance_to(pos2) < stop_distance:
+	#if parent.global_position.distance_to(pos1) < stop_distance || parent.global_position.distance_to(pos2) < stop_distance:
+		#done = true
+		#parent.velocity = Vector3.ZERO
+	if parent.global_position.is_equal_approx(target_position):
+		print("reached target position")
 		done = true
-		parent.velocity = Vector3.ZERO
 	
 	return null
 
-func get_new_direction() -> Vector3:
+func get_new_target_position() -> Vector3:
 	
-	# vertical direction
-	if direction:
-		parent.rotation.y = 0 + rotation_offset
-
-	# horizontal direction
-	else:
-		parent.rotation.y = 90 + rotation_offset
+	#path_ray.target_position = parent.global_basis.z * ray_length
+	print("target pos: ", path_ray.target_position)
+	print("pos: ", parent.global_position + Vector3(0,0.5,0))
 	
-	#adjust for rotation offset to create "strafe like" behavior
-	path_ray.rotation.y -= rotation_offset
+	var direction = randi_range(0,3)
 	
-	#check forwards:
-	path_ray.target_position = parent.global_basis.z * ray_length
+	match direction:
+		0:
+			parent.rotation.y = 0
+		1:
+			parent.rotation.y = 90
+		2:
+			parent.rotation.y = 180
+		3: 
+			parent.rotation.y = 270
+		
+	print(parent.rotation.y)
 	
 	if path_ray.is_colliding():
 		pos1 = path_ray.get_collision_point()
 		pos1.y = parent.position.y #make the entity walk on a straight plane
-	else:
-		pos1 = parent.position
+		print("pos1: ", pos1)
 	
-	#check backwards:
-	path_ray.target_position = -parent.global_basis.z * ray_length
-	
-	if path_ray.is_colliding():
-		pos2 = path_ray.get_collision_point()
-		pos2.y = parent.position.y #make the entity walk on a straight plane
-	else:
-		pos2 = parent.position
-	
-	#pick direction based on random weights
-	var mix: float = randf_range(0,1)
-	var vec1: Vector3 = (pos1 - parent.position) * mix
-	var vec2: Vector3 = (pos2 - parent.position) * 1/mix
-	var result: Vector3 = vec1 + vec2
-	var dir = result.normalized()
-	
-	print("pos1: ", pos1)
-	print("pos2: ", pos2)
-	print("dir: ", dir)
-	return dir
+	var next_target_pos: Vector3 = parent.global_position + (pos1 - parent.global_position) * randf_range(0.2,1)
+	print(next_target_pos)
+	return next_target_pos
 
 func target_reached():
 	await get_tree().create_timer(wait_time).timeout
