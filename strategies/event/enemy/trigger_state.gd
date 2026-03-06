bacdc4092d2f5d0414e012433eaf758fa211d3e5
Plagiatus <lukas.scheuerle@web.de>
2026-03-06 10:46:27 +0100

    trigger state fix

--- strategies/event/enemy/trigger_state.gd
+++ strategies/event/enemy/trigger_state.gd
@@ -3,11 +3,11 @@ extends EventStrategy
 ## Triggers a State from the State Machine
 class_name TriggerStateEventStrategy
 
+@export var state_machine: StateMachine
 @export var state_to_execute: State
 
 func _ready() -> void:
 	pass
 
 func execute_event(_data):
-	#get StateMachine .current_state.exit()
-	state_to_execute.enter()
+	state_machine.switch_to_state(state_to_execute)
