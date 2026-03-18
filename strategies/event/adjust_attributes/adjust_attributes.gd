16113d64ad20b47edb6a142049130237262f1aa3
Plagiatus <lukas.scheuerle@web.de>
2026-03-18 15:47:47 +0100

    difficulty scaler

--- strategies/event/adjust_attributes/adjust_attributes.gd
+++ strategies/event/adjust_attributes/adjust_attributes.gd
@@ -7,12 +7,4 @@ class_name AdjustAttributesEventStrategy extends EventStrategy
 func execute_event(_data):
 	for change in changes:
 		if not change: continue
-		if not parent.get(change.attribute): continue
-		
-		match change.operation:
-			"add":
-				parent.set(change.attribute, parent.get(change.attribute) + change.value)
-			"set":
-				parent.set(change.attribute, change.value)
-			"multiply":
-				parent.set(change.attribute, parent.get(change.attribute) * change.value)
+		change.adjust(parent)
