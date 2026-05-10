a0c5d169a08c3fc0b2a16b1c62d7a5ff4a4ac20e
GammaRayTea <raymond.g.n.dinh@gmail.com>
2026-05-10 17:43:20 +0200

    changed the way music looping works and updated music files to accommodate

--- game/sound/music_player/music_player.gd
+++ game/sound/music_player/music_player.gd
@@ -8,26 +8,3 @@ func _init(song: AudioStreamInteractive):
 func start_playback():
 	if stream:
 		play()
-		update_track_tail_volume()
-
-
-
-
-func update_track_tail_volume():	
-	#lower tail volume of previous track
-	#var current_clip_stream = get_current_clip_stream()
-	#if current_clip_stream is AudioStreamSynchronized: 
-		#current_clip_stream.set_sync_stream_volume(1,-60.0)
-	#
-	var timer = get_tree().create_timer(get_current_clip_stream().get_length()+10)
-	await timer.timeout
-
-	var current_clip_stream = get_current_clip_stream()
-	
-	if current_clip_stream is AudioStreamSynchronized: 
-
-		current_clip_stream.set_sync_stream_volume(1,10.0)
-
-
-func get_current_clip_stream() -> AudioStream:
-	return  stream.get_clip_stream(get_stream_playback().get_current_clip_index())
