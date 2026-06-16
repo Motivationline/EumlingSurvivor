cae515a9dc06d3e36f49aeb35437d454ae982c5e
Plagiatus <lukas.scheuerle@web.de>
2026-06-16 11:27:27 +0200

    make timer event strategy accept multiple events

--- strategies/event/timer.gd
+++ strategies/event/timer.gd
@@ -5,13 +5,15 @@ class_name TimerEventStrategy
 ## how long to wait (in seconds) until the given event gets triggered
 @export var wait_time: float = 3
 
-## What event to call once the time is up
-@export var event: EventStrategy
+## What events to call once the time is up
+@export var events: Array[EventStrategy]
 
 func _setup(_parent: Node, _owner: Node):
 	super (_parent, _owner)
-	event._setup(_parent, _owner)
+	for event in events:
+		event._setup(_parent, _owner)
 
 func execute_event(_data):
 	await get_tree().create_timer(wait_time).timeout
-	event.event_triggered(_data)
+	for event in events:
+		event.event_triggered(_data)
