2d505ab3e4be3d24c2fd0420428edf9686b1419f
Plagiatus <lukas.scheuerle@web.de>
2026-06-17 09:16:04 +0200

    show card upgrade path progress

--- game/ui/upgrades/upgrade_option.gd
+++ game/ui/upgrades/upgrade_option.gd
@@ -17,6 +17,13 @@ func setup(upgrade: Upgrade, delay: float = 0.0, sound_pos_offset: float = 0.0):
 	var texture = textures.get(upgrade.type)
 	if texture:
 		card.find_child("UpgradeImage").texture = texture
+	var tracker = card.find_child("Tracker");
+	var tracker_step = Data.game_data.upgrade_path_progress.get(upgrade.path, 0)
+	if tracker:
+		for i in tracker.get_child_count():
+			tracker.get_child(i).visible = i <= tracker_step
+		animate_dot(tracker.get_child(tracker_step))
+
 	
 	focus_entered.connect(_on_focus_entered)
 	focus_exited.connect(_on_focus_exited)
@@ -35,3 +42,10 @@ func _on_focus_entered():
 
 func _on_focus_exited():
 	scale = Vector2(1, 1)
+
+func animate_dot(node: Sprite3D):
+	if not node: return
+	var tween := create_tween()
+	tween.tween_property(node, "scale", node.scale * 2, 1.0)
+	tween.tween_property(node, "scale", node.scale, 1.0)
+	tween.tween_callback(animate_dot.bind(node))
