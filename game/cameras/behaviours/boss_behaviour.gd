cebf6140905c8b0d4ae44304137fbd04a8c26d56
Keros918 <haasleon.s@gmail.com>
2026-06-22 04:14:54 +0200

    improve cameras

--- game/cameras/behaviours/boss_behaviour.gd
+++ game/cameras/behaviours/boss_behaviour.gd
@@ -9,8 +9,8 @@ extends CameraBehaviour
 @export var boss_size: float = 2.0
 ## The camera will position itself between the boss and the player.
 ## With 0.5 the camera will be exactly in the middle.
-## With 0.0 the camera will be exactly over the boss.
-## With 1.0 the camera will be exactly over the player.
+## With 0.0 the camera will be exactly over the player.
+## With 1.0 the camera will be exactly over the boss.
 @export_range(0.0, 1.0, 0.001) var player_boss_position_ratio: float = 0.5
 ## The minimun size of the camera.
 @export var min_size: float = 5.0
@@ -32,10 +32,13 @@ func setup(camera: GameCamera) -> void:
 
 func update_camera(camera: GameCamera) -> void:
 	if not camera.player or not _boss:
-		camera.switch_to_default_behaviour()
+		camera.switch_to_normal_behaviour()
 		return
 
-	var direction: Vector3 = camera.player.global_position - _boss.global_position
+	var player_position: Vector3 = camera.player.global_position
+	var boss_position: Vector3 = _boss.global_position
+
+	var direction: Vector3 = player_position - boss_position
 	direction.y = 0.0
 	direction = direction.normalized()
 	
@@ -44,20 +47,20 @@ func update_camera(camera: GameCamera) -> void:
 	view_direction = view_direction.normalized()
 	var right_direction := Vector3(-view_direction.z, 0.0, view_direction.x)
 
+	var center: Vector3 = player_position.lerp(boss_position, player_boss_position_ratio)
+	center += view_direction * covered_by_healthbar * camera.size
+
+	var next_position := Vector3(center.x + camera.offset_x, camera.global_position.y, center.z + camera.offset_z)
+	camera.global_position = camera.global_position.lerp(next_position, 0.1)
+	
 	var height_component: float = direction.dot(view_direction)
 	var offset_direction: Vector3 = (height_component * view_direction + direction).normalized()
 
 	var player_offset: Vector3 = offset_direction * player_size
 	var boss_offset: Vector3 = -offset_direction * boss_size
 
-	var expanded_player_position: Vector3 = camera.player.global_position + player_offset
-	var expanded_boss_position: Vector3 = _boss.global_position + boss_offset
-
-	var center: Vector3 = (expanded_player_position + expanded_boss_position) * player_boss_position_ratio
-	center += view_direction * covered_by_healthbar * camera.size
-
-	var next_position := Vector3(center.x + camera.offset_x, camera.global_position.y, center.z + camera.offset_z)
-	camera.global_position = camera.global_position.lerp(next_position, 0.1)
+	var expanded_player_position: Vector3 = player_position + player_offset
+	var expanded_boss_position: Vector3 = boss_position + boss_offset
 
 	var connection_line: Vector3 = expanded_player_position - expanded_boss_position
 	var height: float = absf(connection_line.dot(view_direction))
--- game/cameras/game_camera.gd
+++ game/cameras/game_camera.gd
@@ -8,6 +8,8 @@ extends Camera3D
 var player: CharacterBody3D
 var offset_x = 0.0
 var offset_z = 0.0
+var original_size: float = size
+var return_to_original_size: bool = false
 
 
 func _ready() -> void:
@@ -20,9 +22,15 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	if return_to_original_size:
+		size = lerpf(size, original_size, 0.1)
+	if size == original_size:
+		return_to_original_size = false
+
 	behaviour.update_camera(self)
 
 
 func switch_to_normal_behaviour() -> void:
 	behaviour = NormalBehaviour.new()
 	behaviour.setup(self)
+	return_to_original_size = true
