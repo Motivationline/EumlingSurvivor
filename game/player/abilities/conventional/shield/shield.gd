6ca9135df0c29a7a762ce0fa105d793b57c002fa
Keros918 <haasleon.s@gmail.com>
2026-06-24 14:52:26 +0200

    remove upgrade, change shield max hits

--- game/player/abilities/conventional/shield/shield.gd
+++ game/player/abilities/conventional/shield/shield.gd
@@ -3,12 +3,13 @@ extends StaticBody3D
 ## Protects the entity from projectiles.
 
 ## The amount of projectiles the shield can destroy before being deactivated.
-@export var max_hits: int = 1
+@export var max_hits: Array[int] = []
 ## When true the shield is deactivated at the start.
 @export var disabled: bool = true
 
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
 
+var current_max_hits: int = 0
 var current_hits: int = 0
 
 
@@ -21,6 +22,7 @@ func _ready() -> void:
 
 func activate() -> void:
 	current_hits = 0
+	current_max_hits = max_hits[clampi(Data.game_data.get_eumling_amount(Enum.EUMLING_TYPE.CONVENTIONAL), 0, max_hits.size() - 1)]
 	visible = true
 	collision_shape.disabled = false
 	process_mode = Node.PROCESS_MODE_INHERIT
@@ -34,5 +36,5 @@ func deactivate() -> void:
 
 func hit() -> void:
 	current_hits += 1
-	if current_hits >= max_hits:
+	if current_hits >= current_max_hits:
 		deactivate()
