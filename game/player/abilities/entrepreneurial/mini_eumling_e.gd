611277bf41d1b2f6add15a8271091ba0b7162cec
Plagiatus <lukas.scheuerle@web.de>
2026-06-26 15:13:47 +0200

    change e to attack only once and walk back to player

--- game/player/abilities/entrepreneurial/mini_eumling_e.gd
+++ game/player/abilities/entrepreneurial/mini_eumling_e.gd
@@ -2,23 +2,30 @@ extends MiniEumling
 
 @export var attack_enemy_state: State
 @export var follow_player: State
+@export var reached_player: State
 
 func _ready() -> void:
 	super()
 	Player.player.hit.connect(attack_enemy)
+	reached_player.entered.connect(reset_enemy)
 
 var current_enemy: Enemy
 
 func attack_enemy(enemy: Enemy):
-	
+	if current_enemy: return
 	if enemy.health <= 0: return
 	if current_enemy == enemy: return
 	attack_enemy_state.fixed_target = enemy
 	state_machine.switch_to_state(attack_enemy_state)
-	if current_enemy:
-		current_enemy.died.disconnect(enemy_died)
-	current_enemy = enemy
-	current_enemy.died.connect(enemy_died)
 
-func enemy_died():
+	if not enemy.died.is_connected(stop_attacking_enemy):
+		enemy.died.connect(stop_attacking_enemy)
+	current_enemy = enemy
+
+func stop_attacking_enemy():
 	state_machine.switch_to_state(follow_player)
+	if current_enemy:
+		current_enemy.died.disconnect(stop_attacking_enemy)
+
+func reset_enemy():
+	current_enemy = null
