fb47fb7e81f1e12d6a640778ba1982268d20eaeb
Jussie247 <justus.schultzke@gmail.com>
2026-07-16 11:09:03 +0200

    added scaling to puffer fish + starfish, Added interrupts to pufferfish, debugged in_range.gd with Lukas

--- state_machine/interrupts/conditions/in_range.gd
+++ state_machine/interrupts/conditions/in_range.gd
@@ -9,4 +9,5 @@ class_name InRangeToPlayerInterruptCondition extends InterruptCondition
 func evaluate() -> bool:
 	if not parent or not parent.is_inside_tree(): return false
 	if not Player.player or not Player.player.is_inside_tree(): return false
-	return parent.global_position.distance_to(Player.player.global_position) <= distance * distance
+	prints(parent.global_position.distance_to(Player.player.global_position), distance)
+	return parent.global_position.distance_to(Player.player.global_position) <= distance
