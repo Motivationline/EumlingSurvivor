9ffa2178ba16971b4a7d28a85822d69948d7fed0
plojo <plonjo3@gmail.com>
2026-08-10 09:59:50 +0200

    use @excport in graphics settings

--- game/ui/start_screen/graphics_settings.gd
+++ game/ui/start_screen/graphics_settings.gd
@@ -1,40 +1,41 @@
 extends Control
 
-@onready var render_scale_slider: HSlider = $GridContainer/RenderScaleSlider
-@onready var global_illumination_options: OptionButton = $GridContainer/GlobalIlluminationOptions
-@onready var shadow_options: OptionButton = $GridContainer/ShadowOptions
-@onready var depth_of_field_options: OptionButton = $GridContainer/DepthOfFieldOptions
+@export var render_scale_slider: HSlider
+@export var global_illumination_options: OptionButton
+@export var shadow_options: OptionButton
+@export var depth_of_field_options: OptionButton
 
-@onready var save_button: Button = $GridContainer/SaveButton
-@onready var cancel_button: Button = $GridContainer/CancelButton
+@export var save_button: Button
+@export var cancel_button: Button
 
 var pending_changes := {}
 
 func _ready() -> void:
-	render_scale_slider.value = GraphicsSettings.get_setting(GraphicsSettings.SETTING.RENDER_SCALE)
+	if render_scale_slider:
+		render_scale_slider.value = GraphicsSettings.get_setting(GraphicsSettings.SETTING.RENDER_SCALE)
 
-	global_illumination_options.clear()
-	global_illumination_options.add_item("Off", GraphicsSettings.GLOBAL_ILLUMINATION_OPTIONS.OFF)
-	#global_illumination_options.add_item("Baked", GraphicsSettings.GLOBAL_ILLUMINATION_OPTIONS.BAKED)
-	global_illumination_options.add_item("SSAO", GraphicsSettings.GLOBAL_ILLUMINATION_OPTIONS.SSAO)
+	if global_illumination_options:
+		global_illumination_options.clear()
+		global_illumination_options.add_item("Off", GraphicsSettings.GLOBAL_ILLUMINATION_OPTIONS.OFF)
+		#global_illumination_options.add_item("Baked", GraphicsSettings.GLOBAL_ILLUMINATION_OPTIONS.BAKED)
+		global_illumination_options.add_item("SSAO", GraphicsSettings.GLOBAL_ILLUMINATION_OPTIONS.SSAO)
+		global_illumination_options.select(GraphicsSettings.get_setting(GraphicsSettings.SETTING.GLOBAL_ILLUMINATION))
 
-	global_illumination_options.select(GraphicsSettings.get_setting(GraphicsSettings.SETTING.GLOBAL_ILLUMINATION))
+	if shadow_options:
+		shadow_options.clear()
+		shadow_options.add_item("Off", GraphicsSettings.SHADOW_OPTIONS.OFF)
+		shadow_options.add_item("Low", GraphicsSettings.SHADOW_OPTIONS.LOW)
+		shadow_options.add_item("Medium", GraphicsSettings.SHADOW_OPTIONS.MEDIUM)
+		shadow_options.add_item("High", GraphicsSettings.SHADOW_OPTIONS.HIGH)
+		shadow_options.select(shadow_options.get_item_index(GraphicsSettings.get_setting(GraphicsSettings.SETTING.SHADOW)))
 
-	shadow_options.clear()
-	shadow_options.add_item("Off", GraphicsSettings.SHADOW_OPTIONS.OFF)
-	shadow_options.add_item("Low", GraphicsSettings.SHADOW_OPTIONS.LOW)
-	shadow_options.add_item("Medium", GraphicsSettings.SHADOW_OPTIONS.MEDIUM)
-	shadow_options.add_item("High", GraphicsSettings.SHADOW_OPTIONS.HIGH)
-	
-	shadow_options.select(shadow_options.get_item_index(GraphicsSettings.get_setting(GraphicsSettings.SETTING.SHADOW)))
-
-	depth_of_field_options.clear()
-	depth_of_field_options.add_item("Off", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.OFF)
-	depth_of_field_options.add_item("Low", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.LOW)
-	depth_of_field_options.add_item("Medium", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.MEDIUM)
-	depth_of_field_options.add_item("High", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.HIGH)
-	
-	depth_of_field_options.select(depth_of_field_options.get_item_index(GraphicsSettings.get_setting(GraphicsSettings.SETTING.DEPTH_OF_FIELD)))
+	if depth_of_field_options:
+		depth_of_field_options.clear()
+		depth_of_field_options.add_item("Off", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.OFF)
+		depth_of_field_options.add_item("Low", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.LOW)
+		depth_of_field_options.add_item("Medium", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.MEDIUM)
+		depth_of_field_options.add_item("High", GraphicsSettings.DEPTH_OF_FIELD_OPTIONS.HIGH)
+		depth_of_field_options.select(depth_of_field_options.get_item_index(GraphicsSettings.get_setting(GraphicsSettings.SETTING.DEPTH_OF_FIELD)))
 
 	GraphicsSettings.setting_changed.connect(on_setting_changed)
 	update_buttons()
@@ -53,8 +54,10 @@ func on_setting_changed(setting, value) -> void:
 
 func update_buttons() -> void:
 	var has_unsaved_changes = GraphicsSettings.has_unsaved_changes()
-	cancel_button.disabled = not has_unsaved_changes
-	save_button.disabled = not has_unsaved_changes
+	if cancel_button:
+		cancel_button.disabled = not has_unsaved_changes
+	if save_button:
+		save_button.disabled = not has_unsaved_changes
 
 func _on_render_scale_slider_value_changed(value: float) -> void:
 	GraphicsSettings.set_setting(GraphicsSettings.SETTING.RENDER_SCALE, value)
